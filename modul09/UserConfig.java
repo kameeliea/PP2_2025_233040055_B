4993f58523d78695049e018233ae3e14213fd5a5
kameeliea <kamiliyamufidah16@gmail.com>
2025-12-17 00:27:53 +0700

    Latihan Modul 9

---
+++ modul09/UserConfig.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.pp2.modul09;
+import java.io.Serializable;
+
+public class UserConfig implements Serializable {
+    private String username;
+    private int fontSize;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+}
